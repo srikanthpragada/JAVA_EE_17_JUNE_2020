f51c71ae7b5755881b8c009e95bd95e582f8c324
srikanthpragada <srikanthpragada@gmail.com>
2020-06-20 20:58:19 +0530

    20-JUNE

--- jdbcdemo/src/mysql/TestConnection.java
+++ jdbcdemo/src/mysql/TestConnection.java
@@ -6,6 +6,7 @@ public class TestConnection {
 
 	public static void main(String[] args) throws Exception {
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hr","root","mysql");
+		System.out.println(con.getClass());
 		System.out.println("Connected To HR Database of MySQL!");
 		con.close();
 	}
