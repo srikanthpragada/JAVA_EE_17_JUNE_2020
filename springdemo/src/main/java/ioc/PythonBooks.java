b4984d153df36e76cb42d8b6de671c44e5634808
srikanthpragada <srikanthpragada@gmail.com>
2020-07-21 20:58:22 +0530

    21-JULY

--- springdemo/src/main/java/ioc/PythonBooks.java
+++ springdemo/src/main/java/ioc/PythonBooks.java
@@ -3,28 +3,28 @@ package ioc;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
-public class JavaBooks implements Books {
+@Scope(scopeName="prototype")
+public class PythonBooks implements Books {
 
-	public JavaBooks() {
-		System.out.println("JavaBooks()");
+	public PythonBooks() {
+		System.out.println("PythonBooks()");
 	}
 	
 	@Override
 	public List<String> getBooks() {
 		ArrayList<String> books = new ArrayList<>();
-		books.add("Java Comp. Ref");
-		books.add("Thinking in Java");
+		books.add("Python Tricks");
+		books.add("Python Definite Guide");
 		return books;
 	}
 
 	@Override
 	public String getTopic() {
-		return "Java";
+		return "Python";
 	}
 
 }
